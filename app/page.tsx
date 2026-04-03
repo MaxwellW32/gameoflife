cf1f8fab5e5828c93fd0af820cf41f9ada96f22e
MaxwellW32 <maxwellwedderburn32@gmail.com>
2026-04-02 20:32:52 -0500

    nice

--- app/page.tsx
+++ app/page.tsx
@@ -1,65 +1,98 @@
-import Image from "next/image";
+"use client"
+import React, { useRef } from 'react'
+import { v4 } from 'uuid'
+import styles from "./page.module.css"
+
+type tileType = {
+    id: string,
+    xIndex: number,
+    yIndex: number,
+    type: "white" | "black"
+}
 
 export default function Home() {
-  return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.tsx file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+    const tileSize = 40
+    const tileCount = 10
+
+    const tileContRef = useRef<HTMLDivElement | null>(null)
+    const tiles = useRef<tileType[]>([])
+
+    function generateTiles() {
+        if (tileContRef === null) return
+
+        const xyIndexObj: { [key: string]: boolean } = {}
+
+        //map all positions
+        for (let indexRow = 0; indexRow < tileCount; indexRow++) {//y
+            for (let indexColumn = 0; indexColumn < tileCount; indexColumn++) {//x
+                xyIndexObj[`${indexColumn}_${indexRow}`] = false
+            }
+        }
+
+        const amt = 10
+
+        for (let index = 0; index < amt; index++) {
+            const falseXyIndexObjKeys = Object.entries(xyIndexObj).filter(eachEntry => !eachEntry[1]).map(eachEntry => eachEntry[0])
+            if (falseXyIndexObjKeys.length === 0) continue
+
+            const randomXYKey = falseXyIndexObjKeys[Math.floor(Math.random() * falseXyIndexObjKeys.length)]
+            const [randomXPosition, randomYPosition] = randomXYKey.split("_").map(each => parseInt(each))
+
+            //mark as used position from xyIndexObj
+            xyIndexObj[randomXYKey] = true
+
+            const newTile: tileType = {
+                id: v4(),
+                xIndex: randomXPosition,
+                yIndex: randomYPosition,
+                type: "black"
+            }
+
+            tiles.current.push(newTile)
+        }
+
+        console.log(`$xyIndexObj`, xyIndexObj);
+
+        drawTiles()
+    }
+
+    function drawTiles() {
+        if (tileContRef.current === null) return
+
+        //reset children
+        tileContRef.current.innerHTML = ""
+
+        tiles.current.map(eachTile => {
+            const newHtmlElement = document.createElement("div")
+
+            //add id
+            newHtmlElement.id = eachTile.id
+
+            //styling
+            newHtmlElement.classList.add(styles.tile)
+            newHtmlElement.style.width = `${tileSize}px`
+            newHtmlElement.style.height = `${tileSize}px`
+            newHtmlElement.style.top = `${eachTile.yIndex * tileSize}px`
+            newHtmlElement.style.left = `${eachTile.xIndex * tileSize}px`
+            newHtmlElement.style.backgroundColor = eachTile.type === "black" ? "#000" : "#fff"
+
+            tileContRef.current!.appendChild(newHtmlElement)
+        })
+    }
+
+    return (
+        <div style={{ display: "grid", gridTemplateRows: "auto 1fr", overflow: "auto" }}>
+            <div>
+                <h1>Game Of Life</h1>
+
+                <button className='button2'
+                    onClick={generateTiles}
+                >generate</button>
+            </div>
+
+            <div style={{ display: "grid", overflow: "auto" }}>
+                <div ref={tileContRef} style={{ width: `${tileSize * tileCount}px`, height: `${tileSize * tileCount}px`, position: "relative", backgroundColor: "gray" }}></div>
+            </div>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
-    </div>
-  );
+    )
 }
